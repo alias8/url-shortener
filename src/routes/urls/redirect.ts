b456b1b3a2d92d69ac747d19f58626de002e6a04
James Kirk <jameskirk@gmail.com>
2026-04-10 16:31:59 +1000

    redis cahce

--- src/routes/urls/redirect.ts
+++ src/routes/urls/redirect.ts
@@ -1,18 +1,24 @@
 import { Request, Response, Router } from 'express';
 import { prisma } from '../../db/prisma';
+import { redisPublish } from '../../server';
 
 const router = Router();
-
+const ONE_DAY = 60 * 60 * 24;
 router.get('/:shortCode', async (req: Request, res: Response) => {
   const shortUrl = req.params.shortCode as string;
   if (!shortUrl) {
     return res.status(404).json({ error: `Need shortUrl in query` });
+  }
+  const redisCache = await redisPublish.get(shortUrl);
+  if (redisCache) {
+    return res.redirect(redisCache);
   }
   try {
     const savedUrl = await prisma.url.findFirst({
       where: { short_url: shortUrl },
     });
     if (savedUrl) {
+      redisPublish.set(shortUrl, savedUrl.long_url, 'EX', ONE_DAY);
       return res.redirect(savedUrl.long_url);
     }
     return res.status(404).json({ error: 'Short url not found' });
